8ec88fb771f1d80644c9011eb73de2f458a9c0b6
xd88021as <s560798212@gmail.com>
2024-05-02 12:55:20 +0800

    feat: add eslint and update setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,33 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
+  "env": {
+      "browser": true,
+      "es2021": true
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+  "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended"
   ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
+  "overrides": [
+      {
+          "env": {
+              "node": true
+          },
+          "files": [
+              ".eslintrc.{js,cjs}"
+          ],
+          "parserOptions": {
+              "sourceType": "script"
+          }
+      }
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+      "ecmaVersion": "latest",
+      "sourceType": "module"
   },
-  ignorePatterns: ['.eslintrc.js'],
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "rules": {
+  }
+}
